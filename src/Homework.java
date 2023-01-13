dc3ed13a88cacc59e4b05886f5176cd00d913149
dimab <dima.bogdashkin@gmail.com>
2023-01-13 18:50:14 +0300

    Initial commit

--- src/Homework.java
+++ src/Homework.java
@@ -3,6 +3,11 @@ public class Homework {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1() {
@@ -51,5 +56,54 @@ public class Homework {
         System.out.println(paper);
         paper = paper - 7639;
         System.out.println(paper);
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var totalWeight = boxerWeight1 + boxerWeight2;
+        System.out.println(totalWeight);
+        var difference = boxerWeight2 - boxerWeight1;
+        System.out.println(difference);
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var difference1 = boxerWeight2 - boxerWeight1;
+        System.out.println(difference1);
+        var difference2 = boxerWeight2 % boxerWeight1;
+        System.out.println(difference2);
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        var totalHours1 = 640;
+        var workingHours = 8;
+        var employees = totalHours1 / workingHours;
+        System.out.println("Всего работников в компании — " + employees + " человек");
+        var moreEmployees = employees + 94;
+        var totalHours2 = moreEmployees * workingHours;
+        System.out.println("Если в компании работает " + moreEmployees + " человек, то всего " + totalHours2 + " часов работы может быть поделено между сотрудниками");
     }
 }
